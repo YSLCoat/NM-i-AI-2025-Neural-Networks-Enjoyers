9d3834a01203b201ca9bbbedace7bb155f1c7177
twaal <theodor.walberg@gmail.com>
2025-08-06 16:17:14 +0200

    model_6_5

--- tumor-segmentation/train_unet.py
+++ tumor-segmentation/train_unet.py
@@ -70,14 +70,14 @@ def validate(model, val_loader, device):
 def train():
     model_name = 'model_6_5'
     epochs = 5
-    batch_size = 1
+    batch_size = 4
     resize_shape = (512, 512)
     learning_rate = 1e-4
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    train_images = sorted(glob("tumor-segmentation/datasets/train_augmented_2000/imgs/*.png"))
-    train_masks = sorted(glob("tumor-segmentation/datasets/train_augmented_2000/labels/*.png"))
+    train_images = sorted(glob("tumor-segmentation/datasets/original_augmented_5000/imgs/*.png"))
+    train_masks = sorted(glob("tumor-segmentation/datasets/original_augmented_5000/labels/*.png"))
 
     val_images = sorted(glob("tumor-segmentation/datasets/val_2/imgs/*.png"))
     val_masks = sorted(glob("tumor-segmentation/datasets/val_2/labels/*.png"))
