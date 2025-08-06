03466221ad33b14c3ffc7781e942e56f329c23f2
YSLCoat <torstein.forseth@outlook.com>
2025-08-06 11:01:20 +0200

    port 8000

--- race-car/api_sb3.py
+++ race-car/api_sb3.py
@@ -28,7 +28,7 @@ MODEL_PATH = os.path.join(MODEL_SAVE_DIR, MODEL_BASENAME)
 VECNORM_PATH = os.path.join(MODEL_SAVE_DIR, f"{MODEL_BASENAME}_vecnormalize.pkl")
 
 HOST = os.environ.get("HOST", "0.0.0.0")
-PORT = int(os.environ.get("PORT", "9052"))
+PORT = int(os.environ.get("PORT", "8000"))
 
 # Match training
 N_STACK = 3            # VecFrameStack(n_stack=3) in train
