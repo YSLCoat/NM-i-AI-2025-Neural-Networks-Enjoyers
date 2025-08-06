b56f3bef36ddcfc3c17e2575201ec17c7ae614d1
twaal <theodor.walberg@gmail.com>
2025-08-06 14:38:37 +0200

    save the validation images from remote

--- tumor-segmentation/api.py
+++ tumor-segmentation/api.py
@@ -1,7 +1,10 @@
 import uvicorn
 import time
 import datetime
+import os
 import numpy as np
+import imageio
+
 from fastapi import Body, FastAPI
 from dtos import TumorPredictRequestDto, TumorPredictResponseDto
 # from example import predict
@@ -17,11 +20,20 @@ PORT = 8000
 app = FastAPI()
 start_time = time.time()
 
+
 @app.post('/predict', response_model=TumorPredictResponseDto)
 def predict_endpoint(request: TumorPredictRequestDto):
-
     # Decode request str to numpy array
     img: np.ndarray = decode_request(request)
+
+    # Save image locally
+    save_dir = "received_images"
+    os.makedirs(save_dir, exist_ok=True)
+
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+    filename = f"image_{timestamp}.png"
+    image_path = os.path.join(save_dir, filename)
+    imageio.imwrite(image_path, img)
 
     # Obtain segmentation prediction
     predicted_segmentation = predict(img)
@@ -52,10 +64,7 @@ def index():
     return "Your endpoint is running!"
 
 
-
-
 if __name__ == '__main__':
-
     uvicorn.run(
         'api:app',
         host=HOST,
